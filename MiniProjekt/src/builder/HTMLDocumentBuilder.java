6a02b8eda72c734031e70e557a6e3a58cf4589f5
GTSA - Infinity <williamlindahl03@gmail.com>
2025-02-21 15:07:21 +0100

    added builders

---
+++ MiniProjekt/src/builder/HTMLDocumentBuilder.java
@@ -0,0 +1,44 @@
+package builder;
+
+import java.util.List;
+
+import document.Document;
+import factory.ConcreteElementFactory;
+import factory.DocumentElementFactory;
+
+public class HTMLDocumentBuilder  implements DocumentBuilder {
+
+	private Document document;
+	private DocumentElementFactory factory;
+	
+	public HTMLDocumentBuilder(DocumentElementFactory factory) {
+		this.document = new Document();
+		this.factory = factory;
+	}
+
+	@Override
+	public void addHeading(String text, int level) {
+		document.addElement(factory.createHeading(text, level));
+	}
+
+	@Override
+	public void addParagraph(String text) {
+		document.addElement(factory.createParagraph(text));		
+	}
+
+	@Override
+	public void addList(List<String> items) {
+		document.addElement(factory.createList(items));
+	}
+
+	@Override
+	public Document getResult() {
+		return document;
+	}
+
+	@Override
+	public void addTable(List<List<String>> items) {
+	document.addElement(factory.createTable(items));
+	}
+	
+}
